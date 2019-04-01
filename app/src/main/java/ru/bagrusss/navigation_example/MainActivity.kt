f28264482199d5a1d874d1da3080f13a2894f6b3
Vladislav Kozhushko <bagrusss@gmail.com>
2019-04-02 02:58:48 +0300

    setup navigation

--- app/src/main/java/ru/bagrusss/navigation_example/MainActivity.kt
+++ app/src/main/java/ru/bagrusss/navigation_example/MainActivity.kt
@@ -2,11 +2,31 @@ package ru.bagrusss.navigation_example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var controller: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        controller = navHost.navController.apply {
+            restoreState(savedInstanceState?.getBundle(NAV_STATE))
+            setGraph(R.navigation.main)
+        }
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBundle(NAV_STATE, controller.saveState())
+    }
+
+    companion object {
+        private const val NAV_STATE = "NAV_STATE"
+    }
+
 }
--- app/src/main/java/ru/bagrusss/navigation_example/fragments/StartFragment.kt
+++ app/src/main/java/ru/bagrusss/navigation_example/fragments/StartFragment.kt
@@ -1,5 +1,9 @@
 package ru.bagrusss.navigation_example.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.navigation.Navigation
 import ru.bagrusss.navigation_example.R
 
 /**
@@ -8,5 +12,20 @@ import ru.bagrusss.navigation_example.R
 class StartFragment: BaseFragment() {
 
     override val layout = R.layout.fragment_start
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val nextButton = view.findViewById<Button>(R.id.action_start_to_next)
+
+        /*disposables += RxView.clicks(nextButton)
+                             .subscribe {
+                                 Navigation.findNavController(nextButton)
+                                           .navigate(R.id.action_start_to_next)
+                             }*/
+
+        nextButton.setOnClickListener {
+            Navigation.findNavController(nextButton)
+                      .navigate(it.id)
+        }
+    }
 
 }
