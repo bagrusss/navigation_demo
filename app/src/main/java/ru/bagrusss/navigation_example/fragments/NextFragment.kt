c9fce4dd96894ce0e386907f6a0d7acdd266ba92
Vladislav Kozhushko <bagrusss@gmail.com>
2019-04-02 17:45:08 +0300

    receive args

--- app/src/main/java/ru/bagrusss/navigation_example/fragments/NextFragment.kt
+++ app/src/main/java/ru/bagrusss/navigation_example/fragments/NextFragment.kt
@@ -1,5 +1,9 @@
 package ru.bagrusss.navigation_example.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import ru.bagrusss.navigation_example.MainActivity
 import ru.bagrusss.navigation_example.R
 
 /**
@@ -8,5 +12,15 @@ import ru.bagrusss.navigation_example.R
 class NextFragment : BaseFragment() {
 
     override val layout = R.layout.fragment_next
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        arguments?.let {
+            NextFragmentArgs.fromBundle(it).run {
+                (requireActivity() as MainActivity).supportActionBar?.title = title
+                val textView = view.findViewById<TextView>(R.id.body)
+                textView.append(" id = $itemId")
+            }
+        }
+    }
 
 }
--- app/src/main/java/ru/bagrusss/navigation_example/fragments/StartFragment.kt
+++ app/src/main/java/ru/bagrusss/navigation_example/fragments/StartFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.navigation.Navigation
+import ru.bagrusss.navigation_example.MainActivity
 import ru.bagrusss.navigation_example.R
 import ru.bagrusss.navigation_example.utils.plusAssign
 import ru.bagrusss.navigation_example.utils.setupNav
@@ -19,9 +20,8 @@ class StartFragment: BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val nextButton = view.findViewById<Button>(R.id.action_start_to_next)
 
-        val args = NextFragmentArgs.Builder()
+        val args = NextFragmentArgs.Builder("Next title")
                                    .setItemId(100500)
-                                   .setTitle("Next title")
                                    .build()
                                    .toBundle()
 
@@ -33,6 +33,11 @@ class StartFragment: BaseFragment() {
                                  }
         // OnClickListener
         nextButton.setupNav(args)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        (requireActivity() as MainActivity).supportActionBar?.setTitle(R.string.start)
     }
 
 }
